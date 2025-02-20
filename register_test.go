3229b17ecec512cb2323dd74082235b9809031b6
Sean Keery <skibum55@hotmail.com>
2025-02-20 23:37:06 +0000

    debug

--- register_test.go
+++ register_test.go
@@ -50,16 +50,17 @@ func TestIntegration(t *testing.T) { //nolint:paralleltest
 		t.Fatalf("Failed to create Oracle DB container: %v", err)
 	}
 
-	// host, err := oracleContainer.Host(ctx)
-	// if err != nil {
-	// 	t.Error(err)
-	// }
+	host, err := oracleContainer.Host(ctx)
+	if err != nil {
+		t.Error(err)
+	}
 
-	// mappedPort, err := oracleContainer.MappedPort(ctx, "1521/tcp")
-	// if err != nil {
-	// 	t.Error(err)
-		
-	// }
+	mappedPort, err := oracleContainer.MappedPort(ctx, "1521/tcp")
+	if err != nil {
+		t.Error(err)		
+	}
+
+	t.Log("http://%s:%s", host, mappedPort.Port())
 
 	// jdbcDescriptionString := fmt.Sprintf(`(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=%s)(PORT=%s))(CONNECT_DATA=(SERVICE_NAME=FREEPDB1)))`,
 	// 	host, mappedPort.Port())
